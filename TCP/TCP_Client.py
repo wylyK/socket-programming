c3072dc8fdb658e6caecc3dde1e914ce082e844a
wylyK <willychen720360@gmail.com>
2024-10-10 17:59:25 -0400

    update TCP_Client.py

--- TCP/TCP_Client.py
+++ TCP/TCP_Client.py
@@ -5,11 +5,10 @@ fileName = sys.argv[1]
 name = "localhost"
 port = 50001
 
-clientSocket = socket(AF_INET, SOCK_STREAM)
-clientSocket.connect((name, port))
-
 with open(fileName, 'r') as file:
     for line in file:
+        clientSocket = socket(AF_INET, SOCK_STREAM)
+        clientSocket.connect((name, port))
         clientSocket.send(line.encode())
         result = clientSocket.recv(1024).decode()
 
