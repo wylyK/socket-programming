ccf956cc74a74fc0916e18611d3304c0107ce334
wylyK <willychen720360@gmail.com>
2024-10-10 18:20:30 -0400

    update

--- TCP/TCP_Client.py
+++ TCP/TCP_Client.py
@@ -4,15 +4,16 @@ from socket import *
 if len(sys.argv) < 2:
     print("Expecting input file!")
 
-fileName = sys.argv[1]
-name = "localhost"
-port = 50001
+else:
+    fileName = sys.argv[1]
+    name = "localhost"
+    port = 50001
 
-with open(fileName, 'r') as file:
-    for line in file:
-        clientSocket = socket(AF_INET, SOCK_STREAM)
-        clientSocket.connect((name, port))
-        clientSocket.send(line.strip().encode())
-        result = clientSocket.recv(1024).decode()
+    with open(fileName, 'r') as file:
+        for line in file:
+            clientSocket = socket(AF_INET, SOCK_STREAM)
+            clientSocket.connect((name, port))
+            clientSocket.send(line.strip().encode())
+            result = clientSocket.recv(1024).decode()
 
-clientSocket.close()
+    clientSocket.close()
