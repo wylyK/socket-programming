bcbb003e34b52ab101f1dfb8c8feada041678de9
wylyK <willychen720360@gmail.com>
2024-10-10 19:40:01 -0400

    update

--- TCP/TCP_Client.py
+++ TCP/TCP_Client.py
@@ -11,11 +11,21 @@ else:
 
     with open(fileName, 'r') as file:
         for line in file:
+            
             line = line.strip()
             print("Input request: " + line)
             clientSocket = socket(AF_INET, SOCK_STREAM)
             clientSocket.connect((name, port))
             clientSocket.send(line.encode())
-            result = clientSocket.recv(1024).decode()
+            extendedResult = clientSocket.recv(1024).decode()
+            statusCode, result = extendedResult.split(" ")
+
+            match statusCode:
+                case "200":
+                    print(result)
+                case "620":
+                    print("Invalid operator!")
+                case "630":
+                    print("Invalid operands!")
 
     clientSocket.close()
